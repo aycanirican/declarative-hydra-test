b44c7ab8a3eedcbb1c9f8a75ccc7199863c27c45
Aycan iRiCAN <iricanaycan@gmail.com>
2016-11-29 07:47:53 +0200

    use https backend for github

--- nix/jobsets.nix
+++ nix/jobsets.nix
@@ -27,7 +27,7 @@ let pkgs = (import nixpkgs {});
     '';
 
     mkInputs = branch: ''
-      "src": { "type": "git", "value": "git@github.com:aycanirican/declarative-hydra-test ${branch}", "emailresponsible": false }
+      "src": { "type": "git", "value": "https://github.com/aycanirican/declarative-hydra-test.git ${branch}", "emailresponsible": false }
       "nixpkgs": { "type": "git", "value": "git@github.com:NixOS/nixpkgs-channels 16.09", "emailresponsible": false }
     '';
 
