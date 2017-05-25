bcfe99ef26fa66f0f8964f86106909e96555544a
Aycan iRiCAN <iricanaycan@gmail.com>
2017-05-25 18:59:22 +0300

    Use attributes instead of strings

--- nix/jobsets.nix
+++ nix/jobsets.nix
@@ -1,40 +1,37 @@
-{ prsJSON, nixpkgs, src }:
+{ src, prsJSON, nixpkgs }:
 
 let _pkgs = (import nixpkgs {});
     stableNixpkgs = "https://github.com/NixOS/nixpkgs-channels.git nixos-17.03";
     prs = builtins.fromJSON (builtins.readFile prsJSON);
 
-    mkInputs = info: baseNixpkgs: ''
-      "src":     { "type": "git", "value": "https://github.com/${info.head.repo.owner.login}/${info.head.repo.name}.git ${info.head.ref}", "emailresponsible": false },
-      "nixpkgs": { "type": "git", "value": "${baseNixpkgs}", "emailresponsible": false }
-    '';
+    mkInputs = info: baseNixpkgs: {
+      src     = { type = "git"; value = "https://github.com/${info.head.repo.owner.login}/${info.head.repo.name}.git ${info.head.ref}"; emailresponsible = false; };
+      nixpkgs = { type = "git"; value = "${baseNixpkgs}"; emailresponsible = false; };
+    };
 
-    boilerplate = ''
-      "enabled": 1,
-      "hidden": false,
-      "checkinterval": 5,
-      "schedulingshares": 100,
-      "enableemail": false,
-      "emailoverride": "",
-      "keepnr": 1
-    '';
+    boilerplate = {
+      enabled          = 1;
+      hidden           = false;
+      checkinterval    = 5;
+      schedulingshares = 100;
+      enableemail      = false;
+      emailoverride    = "";
+      keepnr           = 1;
+    };
      
-    PRToJobset = num: info: ''
-      "PR-${num}": {
-        ${boilerplate},
-        "description": "#${num}",
-        "nixexprinput": "src",
-        "nixexprpath": "nix/hydra.nix",
-        "inputs": {
-          ${mkInputs info stableNixpkgs}
-        }
-      }
-    '';
+    PRToJobset = num: info:
+      boilerplate //
+        { description = "#${num}";
+          nixexprinput = "src";
+          nixexprpath = "nix/hydra.nix";
+          inputs = mkInputs info stableNixpkgs;
+        };
+    };
 
     jobsets = _pkgs.lib.mapAttrs (num: info: PRToJobset num info) prs;
 
 in {
-  jobsets = _pkgs.writeText "jobsets.json" (builtins.toJSON jobsets);
+  jobsets = _pkgs.writeText "jobsets.json" jobsets;
 }
     
 
